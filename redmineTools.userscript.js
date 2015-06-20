9fab4c15ec954bf40d392f731ad3b5e3a562263c
hakier <hakier@g.pl>
2015-06-20 13:47:02 +0200

    redmineTools  + debug  + bindKey

--- redmineTools.userscript.js
+++ redmineTools.userscript.js
@@ -8,10 +8,22 @@
 // @match        http://redmine.dpserver.pl/*
 // @grant        none
 // @require     http://code.jquery.com/jquery-latest.js
+// @require     https://raw.githubusercontent.com/jeresig/jquery.hotkeys/master/jquery.hotkeys.js
 // ==/UserScript==
 
 jQuery(function ($) {
-    var selectOption = function ($element) {
+    var bindKey = function (keys, fn) {
+            $(document).on('keydown', null, keys, fn);
+        },
+        debug = {
+            isEnabled: false,
+            dump: function () {
+                if (this.isEnabled) {
+                    console.log(arguments);
+                }
+            }
+        },
+        selectOption = function ($element) {
             if (!$element.length) {
                 return false;
             }
@@ -78,16 +90,28 @@ jQuery(function ($) {
                     ._createBtnToMarkIssueDone()
                     ._createBtnToMarkIssueInProgress()
                     ._createBtnToHideDoneAndInterrupted();
+
+                bindKey('alt+e', function(event) {
+                    event.preventDefault();
+
+                    showAndScrollTo("update", "issue_notes");
+                });
             },
             _isIssuePage: function () {
-                return location.pathname.match(/^\/issues\/[0-9]*/);
+                var isIssuePage = location.pathname.match(/\/issues\/[0-9]*/);
+
+                debug.dump('_isIssuePage', isIssuePage);
+
+                return isIssuePage;
             },
             _addBtnToCreateListFromNotes: function () {
+                debug.dump('_addBtnToCreateListFromNotes');
+
                 if (!this._isIssuePage()) {
                     return;
                 }
 
-                var buttonHTML = '<span id="create-list-from-notes" class="icon icon-edit checklist-new-only save-new-by-button" style="cursor: pointer;">Utwórz listę z notatki</span>',
+                var $createListFromNoteBtn = $('<span id="create-list-from-notes" class="icon icon-edit checklist-new-only save-new-by-button" style="cursor: pointer;">Create <u>l</u>ist from note</span>'),
                     checkList = {
                         $notes: $('#issue_notes'),
                         getInput: function () {
@@ -96,43 +120,40 @@ jQuery(function ($) {
                         getSubmit: function () {
                             return $('#checklist_form_items > span.checklist-item.new > span.icon.icon-add.checklist-new-only.save-new-by-button');
                         }
+                    },
+                    createListFromNote = function () {
+                        var list = checkList.$notes.val().trim().split('\n');
+
+                        $.each(JSON.parse(JSON.stringify(list)), function (key, val) {
+                            checkList.getInput().val(val);
+                            checkList.getSubmit().click();
+                        });
+
+                        checkList.$notes.val('');
+
+                        $('#create-list-from-notes').remove();
+
+                        checkList.getSubmit().after($createListFromNoteBtn);
                     };
 
-                checkList.getSubmit().after(buttonHTML);
-
-                $('body').on('click', '#create-list-from-notes', function () {
-                    var list = checkList.$notes.val().trim().split('\n');
-
-                    $.each(JSON.parse(JSON.stringify(list)), function (key, val) {
-                        checkList.getInput().val(val);
-                        checkList.getSubmit().click();
-                    });
-
-                    checkList.$notes.val('');
-
-                    $('#create-list-from-notes').remove();
-
-                    checkList.getSubmit().after(buttonHTML);
-                });
+                $('body').on('click', '#create-list-from-notes', createListFromNote);
+                checkList.getSubmit().after($createListFromNoteBtn);
+                bindKey('alt+l', createListFromNote);
             },
             localStorageKeys: {
                 childrenList: 'childrenList',
                 createChildrenUrl: 'createChildrenUrl'
             },
             _addBtnToCreateListOfChildrenFromNotes: function () {
+                debug.dump('_addBtnToCreateListOfChildrenFromNotes');
                 if (!this._isIssuePage()) {
                     return;
                 }
 
                 var $addChildBtn = $('#issue_tree .contextual a:last-child'),
-                    $addChildrenFromNotesBtn = $('<span id="add-children-from-notes" class="icon icon-edit checklist-new-only save-new-by-button" style="cursor: pointer;">Add children from notes</span>'),
-                    $notes = $('#issue_notes');
-
-                $addChildrenFromNotesBtn
-                    .css({
-                        marginRight: 20
-                    })
-                    .on('click', function () {
+                    $addChildrenFromNotesBtn = $('<span id="add-children-from-notes" class="icon icon-edit checklist-new-only save-new-by-button" style="cursor: pointer;">Add <u>c</u>hildren from notes</span>'),
+                    $notes = $('#issue_notes'),
+                    createChildrenFromNotes = function () {
                         var list = $notes.val().trim().split('\n'),
                             createChildrenUrl = $addChildBtn.attr('href');
 
@@ -150,11 +171,19 @@ jQuery(function ($) {
                             $notes.val('');
                             location.href = createChildrenUrl;
                         }
-                    });
+                    };
+
+                $addChildrenFromNotesBtn
+                    .css({
+                        marginRight: 20
+                    })
+                    .on('click', createChildrenFromNotes);
 
                 $addChildBtn.before($addChildrenFromNotesBtn);
+                bindKey('alt+c', createChildrenFromNotes)
             },
             _createChild: function () {
+                debug.dump('_createChild');
                 if (!location.pathname.match('/issues/new')) {
                     return;
                 }
@@ -183,73 +212,79 @@ jQuery(function ($) {
                 //}
             },
             _createBtnToMarkIssueDone: function () {
-                if (!location.pathname.match(/issues\/[0-9]+/)) {
+                debug.dump('_createBtnToMarkIssueDone');
+
+                if (!this._isIssuePage()) {
                     return this;
                 }
 
-                var $btnMarkAsDone = $('<a class="icon icon-save" style="margin-right: 20px; margin-left: 20px; cursor: pointer;">Mark as done</a>');
-
-                $btnMarkAsDone.on('click', function () {
+                var $btnMarkAsDone = $('<a class="icon icon-save" style="margin-right: 20px; margin-left: 20px; cursor: pointer;">Mark as <u>d</u>one</a>'),
+                    markAsDone = function () {
                         redmine.issue
                             .assignToMe()
                             .setRatioDone()
                             .setStatus.inProgress().done();
 
                         redmine.issue.submitForm();
-                    }
-                );
+                    };
 
+                $btnMarkAsDone.on('click', markAsDone);
                 this.$.container.top.prepend($btnMarkAsDone);
+                bindKey('alt+d', markAsDone);
 
                 return this;
             },
             _createBtnToMarkIssueInProgress: function () {
+                debug.dump('_createBtnToMarkIssueInProgress');
+
                 if (!this._isIssuePage()) {
                     return this;
                 }
 
-                var $btnMarkAsInProgress = $('<a class="icon icon-move" style="margin-left: 20px; cursor: pointer;">Mark as in progress</a>');
-
-                $btnMarkAsInProgress.on('click', function () {
+                var $btnMarkAsInProgress = $('<a class="icon icon-move" style="margin-left: 20px; cursor: pointer;">Mark as in <u>p</u>rogress</a>'),
+                    markAsInProgress = function () {
                         redmine.issue
                             .assignToMe()
                             .setStatus.inProgress();
 
                         redmine.issue.submitForm();
-                    }
-                );
+                    };
 
+                $btnMarkAsInProgress.on('click', markAsInProgress);
+                bindKey('alt+p', markAsInProgress);
                 this.$.container.top.prepend($btnMarkAsInProgress);
 
                 return this;
             },
             _createBtnToHideDoneAndInterrupted: function () {
+                debug.dump('_createBtnToHideDoneAndInterrupted');
+
                 if (!this._isIssuePage()) {
                     return this;
                 }
 
-                var $btnHideDoneAndInterrupted = $('<a class="icon icon-del" style="margin-left: 20px; cursor: pointer;">Hide done and interrupted</a>');
+                var $btnHideDoneAndInterrupted = $('<a class="icon icon-del" style="margin-left: 20px; cursor: pointer;"><u>H</u>ide done and interrupted</a>'),
+                    hideDoneAndInterrupted = function () {
+                        var $statuses = $('#issue_tree .issue > td:nth-child(3)');
 
-                $btnHideDoneAndInterrupted.on('click', function () {
-                    var $statuses = $('#issue_tree .issue > td:nth-child(3)');
+                        $statuses.each(function () {
+                            var $this = $(this);
 
-                    $statuses.each(function () {
-                        var $this = $(this);
+                            if ($this.text().match('Zamknięty') || $this.text().match('Przerwany')) {
+                                $this.closest('.issue').hide();
+                            }
+                        });
+                    };
 
-                        if ($this.text().match('Zamknięty') || $this.text().match('Przerwany')) {
-                            $this.closest('.issue').hide();
-                        }
-                    });
-                });
+                $btnHideDoneAndInterrupted.on('click', hideDoneAndInterrupted);
+                this.$.container.top.prepend($btnHideDoneAndInterrupted);
+                bindKey('alt+h', hideDoneAndInterrupted);
 
-                this.$.container.top.prepend(
-                    $btnHideDoneAndInterrupted
-                );
+                debug.dump('this.$.container.top', this.$.container.top);
 
                 return this;
             }
         };
 
     redmine.init();
-})
-;
+});
